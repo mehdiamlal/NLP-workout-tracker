2a24260462d4f41e15e6ddcb14d997e8ec57ffc2
Mehdi Amlal <mehdi.amlal.business@gmail.com>
2022-09-06 17:20:49 +0200

    added Google Sheet saving functionality

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from urllib import response
 import requests
 import os
 
-ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+NTX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 NTX_KEY = os.environ.get("NUTRITIONIX_KEY")
 NTX_APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
 NTX_HEADERS = {
     "x-app-id": NTX_APP_ID,
     "x-app-key": NTX_KEY,
 }
+
+SHEETY_ENDPOINT = "https://api.sheety.co/58e062f93ad029aa35ddb4a64278ea1e/workoutTracker/workouts"
 
 
 natural_language_sentence = input("What exercise have you done today?\n> ")
@@ -20,10 +23,18 @@ query = {
     "age": 21
 }
 
-data = requests.post(url=ENDPOINT, headers=NTX_HEADERS, json=query).json()
+workout = requests.post(url=NTX_ENDPOINT, headers=NTX_HEADERS, json=query).json()
 
-for exercise in data["exercises"]:
+for exercise in workout["exercises"]:
     ex_name = exercise["name"].title()
     ex_duration = exercise["duration_min"]
     burned_calories = exercise["nf_calories"]
-    print(f"Exercise: {ex_name}\nDuration: {ex_duration} min\nCalories Burned: {burned_calories} cal\n")
+    data = {
+        "workout": {
+            "exercise": ex_name,
+            "duration": ex_duration,
+            "caloriesBurned": burned_calories
+        }
+    }
+    response = requests.post(url=SHEETY_ENDPOINT, json=data)
+    print(response.text)
